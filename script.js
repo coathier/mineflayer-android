b2c1c8cd79dcd8b73941e0bcebfd4365cb683185
RustCoat <71529172+RustCoat@users.noreply.github.com>
2022-11-15 15:37:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const mineflayer = require('mineflayer')
 
-
-if (process.argv.length < 4 || process.argv.length > 4) {
-  console.log('Usage : node viewer.js [<name>] [<password>]')
+if (process.argv.length < 3 || process.argv.length > 3) {
+  console.log('Usage : node viewer.js [<name>]')
   process.exit(1)
 }
 
@@ -10,25 +9,21 @@ const bot = mineflayer.createBot({
     host: '127.0.0.1',
     port: '25565',
     username: process.argv[2],
-    password: process.argv[3],
+    password: 'password',
     version: '1.8.9',
-    auth: 'microsoft'
+    auth: 'offline'
 })
 
 let target
 
-bot.on('login', () => {
-    bot.setControlState('sprint', true)
-})
-
 bot.on('death', () => {
-    bot.setControlState('forward', false)
+    bot.clearControlStates()
     target = null
 })
 
 bot.on('entityGone', (dead) => {
     if (dead == target) {
-        bot.setControlState('forward', false)
+        bot.clearControlStates()
         target = null
     }
 })
@@ -36,31 +31,52 @@ bot.on('entityGone', (dead) => {
 bot.on('entityHurt', (hurt) => {
     if (hurt.type == 'player') {
         if (hurt == target) {
-            controlForwardMovement(false)
+            bot.setControlState('forward', false)
+            bot.setControlState('sprint', false)
+            diverseStrafe()
         }
         if (hurt.username == bot.username) {
-            target = bot.nearestEntity(entity => entity.type == 'player')
+            target = bot.nearestEntity(entity => entity.type == 'player' && entity.username != bot.username)
         }
     }
 })
 
 bot.on('physicsTick', () => {
     if (target != null) {
-        bot.lookAt(target.position.offset(0, 1.35, 0), false)
+        bot.lookAt(target.position.offset(0, 1.75, 0), false)
+        bot.setControlState('sprint', true)
+        bot.setControlState('forward', true)
         if (bot.entityAtCursor(maxDistance=3) == target) {
             bot.attack(target)
-        } else {
-            controlForwardMovement(true)
         }
     }
 })
 
 bot.on('chat', (username, message) => {
     if (message == 'stop') {
-        bot.setControlState('forward', false)
+        bot.clearControlStates()
         target = null
     }
+    if (message == 'attack') {
+        target = bot.nearestEntity(entity => entity.type == 'player' && entity.username != bot.username)
+    }
 })
+
+function diverseStrafe() {
+    if (bot.getControlState('left')) {
+        if (Math.random() > 0.25) {
+            bot.setControlState('left', false)
+            bot.setControlState('right', true)
+        }
+    } else if (bot.getControlState('right')) {
+        if (Math.random() > 0.25) {
+            bot.setControlState('right', false)
+            bot.setControlState('left', true)
+        }
+    } else {
+        bot.setControlState('left', true)
+    }
+}
 
 let lastTime = 0
 const bufferTicks = 1.5
