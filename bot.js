35c7511a5b494120adfa09d04bf4b12714dfc478
coathier <pal.david.miskolczi@gmail.com>
2023-10-24 17:56:26 +0200

    Installed packages an Modified bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ if (process.argv.length != 7) {
 
 const options = {
     host: process.argv[5],
-    port: process.argv[6]
+    port: process.argv[6],
     email: process.argv[2],
     password: process.argv[3],
     main: process.argv[4]
@@ -17,30 +17,35 @@ const options = {
 
 createBot(options);
 
-function createBot(const options) {
+function createBot(options) {
     let bot = mineflayer.createBot({
         host: options.host,
         port: options.port,
         username: options.email,
         password: options.password,
-        version: '1.8.9',
-        auth: 'microsoft'
+        version: '1.20.1',
+        auth: 'microsoft',
+        disableChatSigning: false
     })
 
-    bot.on('chat', (username, message) => {
+    bot.on('whisper', (username, message, translate, jsonMsg, matches) => {
         if (username != options.main) return
         const words = message.split(' ')
         if (words.lenght == 0) return
         switch (words[0]) {
+            case 'help':
+                //bot.whisper(options.main, 'Avaliable commands: help, msg, leave')
+                break
             case 'leave':
                 bot.quit()
                 process.exit(1)
                 break
-            case 'say':
+            case 'msg':
+                //TODO: The bot crashes when whispering '/tell'
                 bot.chat(words.slice(1).join(' '))
                 break
             default:
-                console.log('No such command: ' + message)
+                //bot.whisper(options.main, 'No such command: ' + words[0] + ' type help for commands')
         }
     })
 }
